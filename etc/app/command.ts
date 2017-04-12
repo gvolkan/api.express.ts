4e3101d7dcf2750d0b68f832979dd1b189b9cecc
James Ma <k@whiteoranges.com>
2017-04-12 09:31:38 -0400

    Changed: app command format

--- etc/app/command.ts
+++ etc/app/command.ts
@@ -5,13 +5,13 @@ import * as CommandLineArgs from "command-line-args" ;
 /** @exports */
 export const COMMAND : GroupedOutput = CommandLineArgs
 ([
-  { name : "http" , defaultValue : "3080" , type : String } ,
-  { name : "https" , defaultValue : "3443" , type : String } ,
-  { name : "host" , defaultValue : "0.0.0.0" , type : String } ,
+  { name : "http" ,       type : String , defaultValue : "3080" } ,
+  { name : "https" ,      type : String , defaultValue : "3443" } ,
+  { name : "host" ,       type : String , defaultValue : "0.0.0.0" } ,
 
-  { name : "bail" , type : Boolean } ,
-  { name : "compilers" , type : String } ,
+  { name : "bail" ,       type : Boolean } ,
+  { name : "compilers" ,  type : String } ,
   { name : "full-trace" , type : Boolean } ,
-  { name : "require" , type : String } ,
+  { name : "require" ,    type : String } ,
 
 ]) ;
